b36a070913f846258cb0caa06be585fe020c310d
Mark Titulaer <fm120615@telfort.nl>
2020-05-14 21:53:40 +0200

    first commit

---
+++ src/main/java/com/titulaer/webprojects/firstproject/App.java
@@ -0,0 +1,13 @@
+package com.titulaer.webprojects.firstproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
